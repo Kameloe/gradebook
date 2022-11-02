9f3e3ed1ce57d5368ebc26b5fb93d437d31f1bae
KarenU <39932664+Kameloe@users.noreply.github.com>
2022-11-02 01:49:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+
+key = student_scores.keys()
+value = student_scores.values()
+
+
+for key in student_scores:
+  student_grades[key] = student_scores[key]
+
+
+#for value in student_scores:
+#  if student_grades[value] >90:
+#    student_grades[key] = "Fail"
+#  else :
+#    student_grades[value] = "Success"
+
+
+  
+#harry_score = student_scores["Harry"]
+
+#exceeds_expectations = range(80,90)
+
+#if harry_score in exceeds_expectations :
+#  print("Exceeds Expectations")
+#student_grades = 
+#if number = range (91,100) = "Outstanding"
+
+#if y in student_scores:
+#  (91<y<100) = 
+#for x in student_scores:
+  
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
